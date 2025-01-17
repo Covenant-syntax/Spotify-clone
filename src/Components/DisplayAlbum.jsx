66979d5b71f0fa6d42d8beb2dfe6ccee6d5f860f
Covenant-syntax <covenantchris4christ@gmail.com>
2025-01-17 23:00:13 +0100

    useParam

--- src/Components/DisplayAlbum.jsx
+++ src/Components/DisplayAlbum.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
     const {id} = useParams();
-    console.log(id)
+    const albumData = albumsData[id];
+    console.log(albumData)
   return (
     <div className='px-3'>
         <Navbar />
