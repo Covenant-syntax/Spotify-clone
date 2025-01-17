1a157e2d724b5b48508a189934ba88b7f058e4e9
Covenant-syntax <covenantchris4christ@gmail.com>
2025-01-18 00:23:15 +0100

    Album data

--- src/Components/DisplayAlbum.jsx
+++ src/Components/DisplayAlbum.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 
 const DisplayAlbum = () => {
     const {id} = useParams();
     const albumData = albumsData[id];
-    console.log(albumData)
   return (
     <div className='px-3'>
         <Navbar />
+        <div className='flex'>
+          <img className='w-[200px]' src={albumData.image}  />
+          <div className='text-white'>
+            <p>Playlist</p>
+            <h1>{albumData.name}</h1>
+            <p>{albumData.desc}</p>
+            
+            <p> 
+              <img className='w-8 inline-block' src={assets.spotify_logo} /> 
+              <b> Spotify </b> 
+              • 1,234,567 ikes • 
+              <b> 50 songs </b> , about 2hrs 30min
+            </p>
+            
+          </div>
+
+        </div>
       
     </div>
   )
